ccee90cd30f4ccc0206084f19a38ec3d76a41d02
Duncan <loxa10077@gmail.com>
2018-05-04 19:34:49 +0800

    ss

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+class CommentsController < ApplicationController
+
+  def create
+    @message = Message.find(parmas[:message_id])
+    @comment = @message.comments.create(params_comment)
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def params_comment
+    params.require(:comment).permit(:content, :user_id)
+  end
+
+  def find_comment
+    
+  end
+
+  def find_message
+    
+  end
+end
